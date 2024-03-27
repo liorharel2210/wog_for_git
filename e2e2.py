283feb9c70ebb22c61064e9746ce4bc9104a06ad
Harel, Lior <harelkopops@gmail.com>
2024-03-27 16:55:09 +0200

    fix the e2e file and update folder

--- e2e2.py
+++ e2e2.py
@@ -7,24 +7,25 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 def test_scores_service(application_url):
+    global driver
     try:
-        # Initialize the Chrome WebDriver
+
         driver = webdriver.Chrome()
         driver.maximize_window()
 
-        # Open the application URL
+
         driver.get(application_url)
 
-        # Wait for the score element to be present on the webpage
+
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'score')))
 
-        # Find the score element on the web page
+
         score_element = driver.find_element_by_class_name('score')
 
-        # Get the text content of the score element
+
         score_text = score_element.text.strip()
 
-        # Check if the score element content is a number between 1 and 1000
+
         if score_text.isdigit():
             score_value = int(score_text)
             if 1 <= score_value <= 1000:
@@ -43,7 +44,7 @@ def test_scores_service(application_url):
         return False
 
     finally:
-        
+
         driver.quit()
 
 def main_function(application_url):
@@ -52,7 +53,7 @@ def main_function(application_url):
     return 0  # Tests passed, return 0 as exit code
 
 if __name__ == "__main__":
-    # Example usage
-    application_url = "http://localhost:63342/wog_for_git/score_template.html"
+
+    application_url = 'http://localhost:63342/wog_for_git/score_template.html'
     exit_code = main_function(application_url)
     sys.exit(exit_code)
